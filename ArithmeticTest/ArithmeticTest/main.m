ac06fe93be8475e4cef5059d6cd0e1acf0a1618f
apple <>
2018-08-03 10:40:30 +0800

    creat project

---
+++ ArithmeticTest/ArithmeticTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ArithmeticTest
+//
+//  Created by apple on 2018/8/3.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
